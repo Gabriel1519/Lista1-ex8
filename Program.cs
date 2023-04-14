1a8aba19e75b133c59be1e6e9910961d56d1e9d9
Gabriel1519 <130614709+Gabriel1519@users.noreply.github.com>
2023-04-14 20:26:23 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1Ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double C;
+            double F;
+
+            Console.Write("Insira a Temperatura em Graus Celsius: ");
+            C = double.Parse(Console.ReadLine());
+
+           F = (C * 9/5) + 32;
+
+            Console.WriteLine("A Temperatura em fahrenheit é {0}.", F);
+        }
+    }
+}
